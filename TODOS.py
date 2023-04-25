84043ac13530daa5e4380cb1ec64d80dcd5d8155
Metaki <kb_f5@yahoo.com>
2023-04-25 16:34:19 +0100

    Added a TODO file

---
+++ TODOS.py
@@ -0,0 +1,7 @@
+# TODO:
+#           - Write code to put multiple images data on one csv
+#                   - Is this correct or should I just use multiple csv for training, like one csv per image?
+#           - Write code to copy the geojson file when I first get it, like Gabriela said in AI module
+#           - Write code to randomly choose bounding boxes to be used for Training or Validation or Testing
+
+
